ec75aa61c880f5de569cc10f9ed5fc50aafa7c74
harry560bca-del <harry560bca@gmail.com>
2025-09-05 10:46:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// Future ke liye: Dark mode ya animation yaha add kar sakte ho
-console.log("Portfolio Website Loaded!");
+// Typing effect
+var typed = new Typed("#typing", {
+  strings: ["Aspiring Developer", "Java Enthusiast", "Python Coder", "Web Developer"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+// Particles.js background
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 80 },
+    "size": { "value": 3 },
+    "move": { "speed": 2 },
+    "line_linked": { "enable": true }
+  }
+});
+
